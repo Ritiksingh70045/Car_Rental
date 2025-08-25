d82ce3384af10ff8660ba9dcaeba52212e129236
Ritiksingh70045 <ritiksingh70045@gmail.com>
2025-08-26 00:07:05 +0530

    Refactor imports and update allowed origins for CORS

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import connectDB from "./configs/db.js";
-import {connectCloudinary} from "./configs/cloudinary.js";
+import { connectCloudinary } from "./configs/cloudinary.js";
 import userRouter from "./routes/userRoutes.js";
 import ownerRouter from "./routes/ownerRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
@@ -17,7 +17,10 @@ await connectDB();
 await connectCloudinary();
 
 // Middleware
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://car-rental-frontend-amber-iota.vercel.app",
+];
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
